c5bed7628dce1fabe58eea6d7edc0ce789d39c6f
dg488 <dg488@cornell.edu>
2014-09-01 13:26:56 -0400

    Initial Constructors

--- DLinkedList.cpp
+++ DLinkedList.cpp
@@ -3,4 +3,25 @@
 
 #include "DLinkedList.h"
 
-// TODO: Fill in the implementation of the DLinkedList class.
+DLinkedList::DLinkedList() {
+  _head = NULL;
+  _tail = NULL
+  _size = 0;
+}
+
+DLinkedList::DLinkedList(const DlinkedList& original) {
+  // Reference cannot be null 
+  if (original._size == 0) {
+    _head = NULL;
+    _tail = NULL;
+    _size = 0;
+  }
+
+  else {
+    _first = new Node(original._first->getValue());
+
+    Node* traverser = original._first->getNext();
+    Node* newList = _first;
+
+    while (traverser
+
